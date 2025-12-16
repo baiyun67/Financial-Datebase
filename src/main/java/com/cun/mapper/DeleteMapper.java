dd9944f38b93e9f48c7a6a0516ea185bda1844e1
baiyun67 <2248168739@qq.com>
2025-12-16 16:16:08 +0800

    Changes

--- src/main/java/com/cun/mapper/DeleteMapper.java
+++ src/main/java/com/cun/mapper/DeleteMapper.java
@@ -5,6 +5,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface DeleteMapper {
-    @Delete("delete from information where id={id}")
+    @Delete("delete from information where id=#{id}")
     int deleteByPrimaryKey(Integer id);
 }
